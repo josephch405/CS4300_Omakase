54de07472c870dd76c4a24862f71d92364223f4e
Jo Chuang <josephch405@gmail.com>
2019-03-17 18:47:40 -0400

    push

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,23 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Omakase"
+net_ids = [
+    "Ryan Curtis: rec284",
+    "Danyal Motiwalla: djm453",
+    "Jessica Wu: jlw377",
+    "Tyler Ishikawa: tyi3",
+    "Joseph Chuang: jcc436"]
+
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
+    query = request.args.get('search')
+    if not query:
+        data = []
+        output_message = ''
+    else:
+        output_message = "Your search: " + query
+        data = range(5)
+    return render_template('search.html', name=project_name, netids=net_ids, output_message=output_message, data=data)
