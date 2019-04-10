2a70761225357a1b7333356eeb0209c4094a2b6d
Jo Chuang <josephch405@gmail.com>
2019-04-09 21:02:04 -0400

    added port from env

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from app import app, socketio
+import os
+print(os.environ['HOME'])
+
+
+app_port = 5000
+if "PORT" in os.environ:
+    app_port = os.environ["PORT"]
+
 
 if __name__ == "__main__":
-  print("Flask app running at http://0.0.0.0:5000")
-  socketio.run(app, host="0.0.0.0", port=5000)
+    print(f"Flask app running at http://0.0.0.0:{app_port}")
+    socketio.run(app, host="0.0.0.0", port=app_port)
