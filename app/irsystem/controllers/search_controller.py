7abfcb85c6225459ab7e2fa04346ffa75c086e90
Tyler Ishikawa <tyi3@cornell.edu>
2019-04-11 18:02:46 -0400

    update index page and add navbar

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,17 +2,15 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.search import find_best_restaurants, find_best_menu
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-
-project_name = "Omakase"
-net_ids = [
-    "Ryan Curtis: rec284",
-    "Danyal Motiwalla: djm453",
-    "Jessica Wu: jlw377",
-    "Tyler Ishikawa: tyi3",
-    "Joseph Chuang: jcc436"]
+from sqlalchemy.sql.expression import func
 
 
 @irsystem.route('/', methods=['GET'])
+def index():
+    rand_restaurant_name = Restaurant.query.order_by(func.random()).first().name
+    return render_template('index.html', restaurant_suggestion=rand_restaurant_name)
+
+@irsystem.route('/search', methods=['GET'])
 def search():
     query = request.args.get('search')
     if not query:
@@ -26,5 +24,5 @@ def search():
         if menu is not None:
             data = list(menu["name"].values)
         print(len(data))
-    return render_template('search.html', name=project_name, netids=net_ids,
+    return render_template('index.html',
                            output_message=output_message, data=data)
