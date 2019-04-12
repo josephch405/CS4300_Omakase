83f68e43151856cfcfa1e7c4ea14a6dea4a357b1
Tyler Ishikawa <tyi3@cornell.edu>
2019-04-11 20:15:07 -0400

    test

---
+++ app/irsystem/models/data_models.py
@@ -0,0 +1,43 @@
+from . import *
+
+class MenuItem(Base):
+  __tablename__ = 'menu_items'
+
+  name           = db.Column(db.String(128), nullable=False)
+  category       = db.Column(db.String(128), nullable=False)
+  price          = db.Column(db.Float(), nullable=False)
+  restaurant_id  = db.Column(db.Integer(), db.ForeignKey('Restaurant.id'), nullable=False)
+
+  def __init__(self, **kwargs):
+    self.name           = kwargs.get('name', None)
+    self.category       = kwargs.get('category', None)
+    self.price          = kwargs.get('price', None)
+    self.restaurant_id  = kwargs.get('restaurant_id', None)
+
+  def __repr__(self):
+    return str(self.__dict__)
+
+
+class MenuItemSchema(ModelSchema):
+  class Meta:
+    model = MenuItem
+
+class Restaurant(Base):
+  __tablename__ = 'restaurants'
+
+  name           = db.Column(db.String(128), nullable=False, unique=True)
+  url            = db.Column(db.String(128), nullable=False, unique=True)
+
+  menu_items     = db.relationship('MenuItem', backref=backref('menu_items'), lazy=True)
+
+  def __init__(self, **kwargs):
+    self.name           = kwargs.get('name', None)
+    self.url            = kwargs.get('url', None)
+
+  def __repr__(self):
+    return str(self.__dict__)
+
+
+class RestaurantSchema(ModelSchema):
+  class Meta:
+    model = Restaurant
